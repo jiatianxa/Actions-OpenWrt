bcfee3ceb8324a233de39f30d1704e269d56fe87
cqmylx <67597378+cqmylx@users.noreply.github.com>
2020-11-29 18:24:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/lean/luci-app-adguardhome
+git clone https://github.com/ujincn/smartdns.git ./package/lean/smartdns
+git clone https://github.com/ujincn/luci-app-smartdns-compat.git ./package/lean/luci-app-smartdns
+wget https://github.com/vernesong/OpenClash/archive/master.zip  #下载OpenClash
+unzip master.zip  #解压
+cp -r OpenClash-master/luci-app-openclash package  #复制OpenClash软件包到OpenWrt
