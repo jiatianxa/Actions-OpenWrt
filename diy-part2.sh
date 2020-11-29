5367f79686bf756a7334e15dccdd61dce7123bc3
cqmylx <67597378+cqmylx@users.noreply.github.com>
2020-11-29 18:25:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
+sed -i 's/set luci.main.mediaurlbase/#set luci.main.mediaurlbase/g' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
